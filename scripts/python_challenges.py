b53fc8fa7198a78b2d593abbd4b8fbc7f34ebd4c
Harshit <harshitsharma14jun@gmail.com>
2025-09-05 18:35:53 +0530

    Update python_challenges.py

--- scripts/python_challenges.py
+++ scripts/python_challenges.py
@@ -276,12 +276,21 @@ else:
 # Building the car game
 
 command = ""
+started = False
 while True:
     command = input(">").lower()
     if command == "start":
-        print("Car started....")
+        if started:
+            print("Car is already started!")
+        else:
+            started = True
+            print("Car started....")
     elif command == "stop":
-        print("Car stopped.")
+        if not started:
+            print("Car is already Stopped!")
+        else:
+            started = False
+            print("Car stopped.")
     elif command == "help":
         print("""
         start - To start the car
